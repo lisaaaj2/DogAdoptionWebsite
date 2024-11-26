3074897a06bc4381bea58af2ab70175d646dfc11
Lisa Johnson <lisa.johnson.4@ndus.edu>
2024-11-26 13:44:24 -0600

    Add project files.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,30 @@
+﻿namespace Dog_Adoption_Website.Data
+{
+    public class AppDBContext
+    {
+        private static List<User> _users = new List<User>();
+
+        public static bool SaveUser(User user)
+        {
+            bool isExist = _users.Any(x => x.Email == user.Email);
+            if (!isExist)
+            {
+                _users.Add(user);
+                return true;
+
+            }
+            return false;
+        }
+
+    }
+
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Dog_Adoption_Website.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/accessdenied";
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
